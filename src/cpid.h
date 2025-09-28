5669a15673318809215490e0718cbd4f0624df5d
jihong <hojindohong@nate.com>
2025-09-28 23:58:52 +0900

    upload

---
+++ src/cpid.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 Bradley J. Snyder <snyder.bradleyj@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef CPID_H
+#define CPID_H
+
+class CPIDImpl;
+class CPID
+{
+
+public:
+    // Kp -  proportional gain
+    // Ki -  Integral gain
+    // Kd -  derivative gain
+    // dt -  loop interval time
+    // max - maximum value of manipulated variable
+    // min - minimum value of manipulated variable
+    CPID( double dt, double max, double min, double Kp, double Kd, double Ki );
+
+    // Returns the manipulated variable given a setpoint and current process value
+    double calculate( double setpoint, double pv );
+    ~CPID();
+
+private:
+    CPIDImpl *pimpl;
+};
+
+class CPIDImpl
+{
+    public:
+        CPIDImpl( double dt, double max, double min, double Kp, double Kd, double Ki );
+        ~CPIDImpl();
+        double calculate( double setpoint, double pv );
+
+    private:
+        double _dt;
+        double _max;
+        double _min;
+        double _Kp;
+        double _Kd;
+        double _Ki;
+        double _pre_error;
+        double _integral;
+};
+#endif // CPID_H
